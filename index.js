a9e53e3097da501eaf6107e8438d6f90e8797c88
GauravThakur-git <146906067+GauravThakur-git@users.noreply.github.com>
2023-11-03 12:50:20 +0530

    Modified code to store data in storage

--- index.js
+++ index.js
@@ -1,61 +1,52 @@
-try{
-    let myLeads = []
-    let inputEl = document.getElementById("input-el")
-    let inputBtn =document.getElementById("input-btn")
-    let ulEl =    document.getElementById("ul-el")
-    let deleteBtn=document.getElementById("delete-btn")
-    let tabBtn =  document.getElementById("tab-btn")
-    let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-    
-    if ( typeof window!== "undefined") {
-    
-    if (leadsFromLocalStorage) {
-        myLeads = leadsFromLocalStorage
-        render(myLeads)
+try {
+    let myLeads = [];
+    let inputEl = document.getElementById("input-el");
+    let inputBtn = document.getElementById("input-btn");
+    let ulEl = document.getElementById("ul-el");
+    let deleteBtn = document.getElementById("delete-btn");
+    let tabBtn = document.getElementById("tab-btn");
+  
+    chrome.storage.local.get({ myLeads: [] }, (result) => {
+      myLeads = result.myLeads;
+      render(myLeads);
+    });
+  
+    tabBtn.addEventListener("click", function () {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url);
+        chrome.storage.local.set({ myLeads: myLeads });
+        render(myLeads);
+      });
+    });
+  
+    function render(leads) {
+      let listItems = "";
+      for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `;
+      }
+      ulEl.innerHTML = listItems;
     }
-    
-    const tabs = []
-    
-    
-    tabBtn.addEventListener("click", function(){
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            myLeads.push(tabs[0].url)
-            localStorage.setItem("myleads", JSON.stringify(myLeads)) 
-            render(myLeads)
-        })
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-        
-    })
-      function render(leads) {
-        let listItems = ""
-        for (let i = 0; i < leads.length; i++) {
-            listItems += `
-                <li>
-                    <a target='_blank' href='${leads[i]}'>
-                        ${leads[i]}
-                    </a>
-                </li>
-            `
-        }
-        ulEl.innerHTML = listItems
-    }
-    
-    deleteBtn.addEventListener("dblclick", function() {
-        localStorage.clear()
-        myLeads = []
-        render(myLeads)
-    })
-    
-    inputBtn.addEventListener("click", function() {
-        myLeads.push(inputEl.value)
-        inputEl.value = ""
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-    
-    }}
-    catch(e)
-    {
-        console.log(e)
-    }
+  
+    deleteBtn.addEventListener("dblclick", function () {
+      chrome.storage.local.remove("myLeads", function () {
+        myLeads = [];
+        render(myLeads);
+      });
+    });
+  
+    inputBtn.addEventListener("click", function () {
+      myLeads.push(inputEl.value);
+      inputEl.value = "";
+      chrome.storage.local.set({ myLeads: myLeads });
+      render(myLeads);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+  
